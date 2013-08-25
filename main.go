d0c1c898244dbc9a83f5734bb94c065622de2e89
hearts <hearts.zhang@gmail.com>
2013-08-25 11:29:14 +0800

    refine gextract.main

--- main.go
+++ main.go
@@ -3,43 +3,34 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"code.google.com/p/go.net/html"
 	"flag"
-	"fmt"
 	htmldoc "gextract/document"
-	"io/ioutil"
 	"log"
 	"os"
 )
 
 var (
-	html_file = flag.String("f", "1.html", "the html file path")
-	out       = flag.String("o", "5.html", "the cleaned-up filepath")
-	url       = flag.String("u", "", "the online web page")
+	//	html_file = flag.String("f", "1.html", "the html file path")
+	//	out = flag.String("o", "5.html", "the cleaned-up filepath")
+	url = flag.String("u", "", "the online web page")
 )
 
 func main() {
 	flag.Parse()
-	if len(*html_file) == 0 {
-		fmt.Println("html_file is required")
-		return
-	}
-	os.Remove("2.html")
-	os.Remove("3.html")
-	os.Remove("4.html")
-	os.Remove("5.html")
-
+	htmlfile := ""
 	if len(*url) != 0 {
-		os.Remove(*html_file)
-		htmldoc.FetchUrl(*url, *html_file)
+		htmlfile, _ = htmldoc.FetchUrl2(*url)
+		log.Println("writing step 1", htmlfile)
+
 	}
-	// data, err := ioutil.ReadFile(*html_file)
-	f, err := os.Open(*html_file)
+
+	f, err := os.Open(htmlfile)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
+
 	reader := bufio.NewReader(f)
 	doc, err := html.Parse(reader)
 	if err != nil {
@@ -47,12 +38,13 @@ func main() {
 		return
 	}
 
-	cleaner := htmldoc.NewHtmlCleaner()
+	cleaner := htmldoc.NewHtmlCleaner(*url)
 	log.Println("prepare cleaning doc")
 	cleaner.CleanHtml(doc)
 	log.Println(cleaner)
 
-	write_html(doc, "2.html")
+	//	h2ml := write_html(doc)
+	log.Println("writing step 2", htmldoc.WriteHtmlFile2(doc))
 
 	//	doc1, _ := htmldoc.FlattenHtmlDocument(cleaner.Article)
 	//	write_html(doc1, "3.html")
@@ -61,26 +53,18 @@ func main() {
 
 	doc1, article := rdr.CreateArticle()
 
-	write_html(doc1, "3.html")
+	//	write_html(doc1, "3.html")
+	log.Println("writing step 3", htmldoc.WriteHtmlFile2(doc1))
 
 	boiler := htmldoc.NewBoilerpiper(article)
 	boiler.NumberWordsRulesFilter()
 
-	write_html(doc1, "4.html")
+	//	write_html(doc1, "4.html")
+	h4ml := htmldoc.WriteHtmlFile2(doc1)
+	log.Println("writing step 4", h4ml)
 
 	boiler.FormPrefixFilter()
-	write_html(doc1, *out)
-
-}
-
-func write_html(doc *html.Node, fp string) {
-	data := new(bytes.Buffer)
-	if err := html.Render(data, doc); err != nil {
-		panic(err)
-	}
-	// fmt.Println(data.String())
-	if err := ioutil.WriteFile(fp, data.Bytes(), 0644); err != nil {
-		panic(err)
-	}
-
+	//	write_html(doc1, *out)
+	h5ml := htmldoc.WriteHtmlFile2(doc1)
+	log.Println("writing step 5", h5ml)
 }
