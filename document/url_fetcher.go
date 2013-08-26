afd17dd95bd617d4460b132123d9c01393558620
hearts <hearts.zhang@gmail.com>
2013-08-26 11:02:16 +0800

    redefine FetchUrl2
    - FetchUrl2 will return content-type. content-type like application/xml
    can be used later for determining doc format
    - set http dial time to 8 seconds. fetching url from blocked website
    should encounter a very long waiting time
    - try a second chance to download url via goagent proxy.
    - add feature FetchUrl3. FetchUrl3 will return error nor panic the error

--- document/url_fetcher.go
+++ document/url_fetcher.go
@@ -4,42 +4,98 @@ import (
 	"bufio"
 	"bytes"
 	"code.google.com/p/go.net/html"
+	"compress/flate"
 	"compress/gzip"
+	"errors"
 	iconv "github.com/djimenez/iconv-go"
 	"io"
 	"io/ioutil"
 	"log"
+	"mime"
+	"net"
 	"net/http"
+	"net/url"
 	"os"
-	"regexp"
+	"time"
+	//	"regexp"
 	"strings"
 )
 
-// return utf-8 encoded cache file path
-func FetchUrl2(url string) string {
-	client := &http.Client{}
+const proxy_address = "http://localhost:8087"
+
+func new_proxy_transport(pxyaddr string) *http.Transport {
+	pxy, _ := url.Parse(pxyaddr)
+	return &http.Transport{Proxy: http.ProxyURL(pxy), Dial: timeout_dialer}
+}
+
+var timeout = time.Duration(8 * time.Second)
+
+func timeout_dialer(network, addr string) (net.Conn, error) {
+	return net.DialTimeout(network, addr, timeout)
+}
+
+func do_request_client(client *http.Client, url string) (*http.Response, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	try_panic(err)
 
 	req.Header.Add("Accept-Encoding", "gzip, deflate")
 
-	resp, err := client.Do(req)
+	return client.Do(req)
+}
+func do_request(url string) (*http.Response, error) {
+	client := &http.Client{Transport: &http.Transport{Dial: timeout_dialer}}
+	resp, err := do_request_client(client, url)
+	if err != nil {
+		log.Println(err)
+		client = &http.Client{Transport: new_proxy_transport(proxy_address)}
+		return do_request_client(client, url)
+	}
+
+	return resp, err
+}
+
+func fetch_url(url string) (string, string) {
+	/*	req, err := http.NewRequest("GET", url, nil)
+		try_panic(err)
+
+		req.Header.Add("Accept-Encoding", "gzip, deflate")
+
+		resp, err := client.Do(req)j
+	*/
+	resp, err := do_request(url)
 	try_panic(err)
 
 	defer resp.Body.Close()
 
-	var reader io.Reader = nil
-	ct := extract_charset(resp.Header.Get("content-type"))
-	log.Println("content-type:", ct)
-
-	ce := strings.ToLower(resp.Header.Get("content-encoding"))
-	if strings.Contains(ce, "gzip") || strings.Contains(ce, "deflate") {
-		reader, err = gzip.NewReader(resp.Body)
-		try_panic(err)
-	} else {
-		reader = resp.Body
+	charset, content_type := extract_charset(resp.Header.Get("content-type"))
+	if resp.StatusCode != http.StatusOK {
+		try_panic(errors.New(resp.Status + " " + http.StatusText(resp.StatusCode)))
 	}
 
+	log.Println(resp.Header.Get("Content-Encoding"))
+
+	var reader io.Reader = nil
+	ce := strings.ToLower(resp.Header.Get("content-encoding"))
+	switch ce {
+	default:
+		reader = resp.Body
+	case "gzip":
+		reader, err = gzip.NewReader(resp.Body)
+		try_panic(err)
+	case "deflate":
+		rd := flate.NewReader(resp.Body)
+		defer rd.Close()
+		reader = rd
+	}
+
+	/*
+		if strings.Contains(ce, "gzip") || strings.Contains(ce, "deflate") {
+			reader, err = gzip.NewReader(resp.Body)
+			try_panic(err)
+		} else {
+			reader = resp.Body
+		}
+	*/
 	of, err := ioutil.TempFile("", "")
 	try_panic(err)
 	defer of.Close()
@@ -48,26 +104,26 @@ func FetchUrl2(url string) string {
 	//  of.Sync()
 	of.Seek(0, 0)
 
-	if len(ct) == 0 {
-		ct = detect_charset(of)
+	if len(charset) == 0 {
+		charset, _ = detect_charset(of)
 	}
-	log.Println("detected content-type", ct)
+	log.Println("detected content-type", charset)
 
 	of.Seek(0, 0)
 	//某些技术网站使用繁体，但标识gb2312
-	if len(ct) == 0 || ct == "gb23123" {
-		ct = "gbk"
+	if len(charset) == 0 || charset == "gb23123" {
+		charset = "gbk"
 	}
 
 	tf, err := ioutil.TempFile("", "")
 	try_panic(err)
 	defer tf.Close()
 
-	if ct != "utf-8" {
+	if charset != "utf-8" {
 		in, _ := ioutil.ReadAll(of)
 		out := make([]byte, len(in)*2)
 
-		_, w, _ := iconv.Convert(in, out, ct, "utf-8")
+		_, w, _ := iconv.Convert(in, out, charset, "utf-8")
 		out = out[:w]
 
 		io.Copy(tf, bytes.NewReader(out))
@@ -78,7 +134,21 @@ func FetchUrl2(url string) string {
 		//		defer tf.Close()
 		io.Copy(tf, of)
 	}
-	return tf.Name()
+	return tf.Name(), content_type
+}
+
+// return utf-8 encoded cache file path
+func FetchUrl3(url string) (tf, content_type string, err error) {
+	defer func() {
+		err, _ = recover().(error)
+	}()
+
+	tf, content_type = fetch_url(url)
+	return
+}
+
+func FetchUrl2(url string) (string, string) {
+	return fetch_url(url)
 }
 
 /*
@@ -146,28 +216,42 @@ func FetchUrl(url string, localpath string) error {
 }
 */
 
-func extract_charset(ct string) string {
-	ct = strings.ToLower(ct)
-	re := regexp.MustCompile(`(?i)(?:charset *= *)([^; ]+)`)
-	charset := re.FindStringSubmatch(ct)
-	if len(charset) == 2 {
-		return charset[1]
-	}
-	return ""
+func extract_charset(ct string) (string, string) {
+	content_type, param, _ := mime.ParseMediaType(ct)
+	//	log.Println(mt, param)
+	return param["charset"], content_type
+	/*
+		ct = strings.ToLower(ct)
+		re := regexp.MustCompile(`(?i)(?:charset *= *)([^; ]+)`)
+		charset := re.FindStringSubmatch(ct)
+		if len(charset) == 2 {
+			return charset[1]
+		}
+		return ""
+	*/
 }
-func detect_charset(file *os.File) string {
-	reader := bufio.NewReader(file)
-	z := html.NewTokenizer(reader)
-	for tt := z.Next(); tt != html.ErrorToken; tt = z.Next() {
-		if tt == html.StartTagToken || tt == html.SelfClosingTagToken {
-			token := z.Token()
-			if ct := detect_charset_by_token(token); len(ct) > 0 {
-				return ct
+func detect_charset(file *os.File) (string, string) {
+	head := make([]byte, 512)
+	file.Read(head)
+	ct := http.DetectContentType(head)
+	return extract_charset(ct)
+	/*
+		file.Seek(0, 0)
+		reader := bufio.NewReader(file)
+		z := html.NewTokenizer(reader)
+		for tt := z.Next(); tt != html.ErrorToken; tt = z.Next() {
+			if tt == html.StartTagToken || tt == html.SelfClosingTagToken {
+				token := z.Token()
+				if ct := detect_charset_by_token(token); len(ct) > 0 {
+					return ct
+				}
 			}
 		}
-	}
-	return ""
+		return ""
+	*/
 }
+
+/*
 func detect_charset_by_token(token html.Token) string {
 	if token.Data == "meta" {
 		log.Println(token)
@@ -189,6 +273,7 @@ func extract_charset_from_meta_content_attr(attrs []html.Attribute) string {
 	}
 	return ""
 }
+*/
 func try_panic(err error) {
 	if err != nil {
 		panic(err)
