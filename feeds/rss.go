b61cf3e4a2822749e9eb9d3029c0deb45b3a34cb
hearts <hearts.zhang@gmail.com>
2013-08-26 11:10:27 +0800

    remove hash function
    - description and content should be the same if they are very sample, so
    we hash content shouldn't work properly
    - add Entry.Created field
    - add ContentStatus and ContentPath fields
    - rename PreSave to PreProcess
    - clean Entry.summary and Entry.Content

--- feeds/rss.go
+++ feeds/rss.go
@@ -2,10 +2,21 @@ package feeds
 
 import (
 	"encoding/xml"
-	"hash/fnv"
+	"gextract/document"
+	//	"hash/fnv"
 	//	"labix.org/v2/mgo/bson"
+	"log"
 	"os"
 	"time"
+)
+
+const (
+	content_status_unresolved = iota
+	content_status_ready      // content has been stored in a file
+	content_status_failed     // fetch content failed
+	content_status_unavail    // fetch content ok, buy extracted-body is low quality
+	content_status_summary    // content stored in Entry.Summary
+	content_status_content    // content stored in Entry.Content
 )
 
 type Category struct {
@@ -39,37 +50,59 @@ type Entry struct {
 	Link    string    `xml:"link" json:"link" bson:"link"`        // required, unique , used as primary key
 	PubDate string    `xml:"pubDate,omitempty" json:"-" bson:"-"` // created or updated
 	Date    time.Time `xml:"-" json:"pubdate" bson:"pubdate"`
-	Digest  int64     `xml:"-" json:"digest,minsize" bson:"digest,minsize"` // fnv1.sum64(summary+content)
-	Read    bool      `xml:"-" json:"read" bson:"read"`
-	Cid     string    `xml:"-" bson:"cid" json:"cid"`
+	Created time.Time `xml:"-" json:"created" bson:"created"`
+
+	//	Digest int64  `xml:"-" json:"digest,minsize" bson:"digest,minsize"` // fnv1.sum64(summary+content)
+	Read bool   `xml:"-" json:"read" bson:"read"`
+	Cid  string `xml:"-" bson:"cid" json:"cid"`
 
 	Category []string `xml:"category" bson:"category,omitempty" json:"category,omitempty"`
 	//http://purl.org/dc/elements/1.1/
-	Author  string `xml:"//http://purl.org/dc/elements/1.1/ creator,omitempty" bson:"author,omitempty" json:"author,omitempty"`
-	Summary string `xml:"description" json:"summary,omitempty" bson:"summary"` // required
-	Content string `xml:"http://purl.org/rss/1.0/modules/content/ encoded" json:"content,omitempty" bson:"content,omitempty"`
-	Guid    string `xml:"guid,omitempty" json:"guid,omitempty" bson:"guid,omitempty"`
+	Author        string `xml:"//http://purl.org/dc/elements/1.1/ creator,omitempty" bson:"author,omitempty" json:"author,omitempty"`
+	Summary       string `xml:"description" json:"summary,omitempty" bson:"summary"` // required
+	Content       string `xml:"http://purl.org/rss/1.0/modules/content/ encoded" json:"content,omitempty" bson:"content,omitempty"`
+	Guid          string `xml:"guid,omitempty" json:"guid,omitempty" bson:"guid,omitempty"`
+	ContentStatus int    `xml:"-" json:"content_status" bson:"content_status"` // full content is ready
+	ContentPath   string `xml:"" bson:"content_path" json:"content_path"`
 }
 
-func (this *Entry) PreSave(cid string, category []string) {
+func (this *Entry) PreProcess(cid string, category []string) {
 	//	this.Id = bson.NewObjectId()
 	this.Date = parse_time1(this.PubDate)
+	this.Created = time.Now()
 	this.Cid = cid
 	if len(this.Guid) == 0 {
 		this.Guid = this.Link
 	}
-	hash := fnv.New64a()
-	hash.Write([]byte(this.Summary))
-	hash.Write([]byte(this.Content))
-	this.Digest = int64(hash.Sum64())
-
+	/*	hash := fnv.New64a()
+		hash.Write([]byte(this.Summary))
+		hash.Write([]byte(this.Content))
+		this.Digest = int64(hash.Sum64())
+	*/
+	log.Println(this.Link)
 	c := make([]string, len(this.Category)+len(category))
 	copy(c, this.Category)
 	copy(c[len(this.Category):], category)
 	this.Category = c
+
+	this.clean_summary()
+
+	switch {
+	default:
+		this.ContentStatus = content_status_unresolved
+	case len(this.Summary) > 512:
+		this.ContentStatus = content_status_summary
+	case len(this.Content) > 512:
+		this.ContentStatus = content_status_content
+	}
 }
 
-func (this *Channel) PreSave(url string) {
+func (this *Entry) clean_summary() {
+	this.Summary, _ = document.CleanFragment(this.Summary, this.Link)
+	this.Content, _ = document.CleanFragment(this.Content, this.Link)
+}
+
+func (this *Channel) PreProcess(url string) {
 	//	this.Id = bson.NewObjectId()
 	this.Date = parse_time1(this.PubDate, this.LastBuildDate)
 	this.Href = this.LinkOne()
@@ -77,7 +110,6 @@ func (this *Channel) PreSave(url string) {
 		this.Href = url
 	}
 	this.Enabled = true
-	this.Refresh = this.Date
 	//  hourly, daily, weekly, monthly, yearly
 	switch this.UpdatePeriod {
 	case "hourly":
@@ -95,6 +127,7 @@ func (this *Channel) PreSave(url string) {
 	if this.Ttl == 0 {
 		this.Ttl = 24 * 60 // minutes
 	}
+	this.Refresh = this.Date.Add(time.Duration(this.Ttl) * time.Minute)
 }
 
 // private wrapper around the RssFeed which gives us the <rss>..</rss> xml
@@ -145,9 +178,9 @@ func NewRss2(filepath string, url string) (Channel, []Entry) {
 	var v rss
 	err = xml.NewDecoder(f).Decode(&v)
 	try_panic(err)
-	v.Channel.PreSave(url)
+	v.Channel.PreProcess(url)
 	for i, _ := range v.Channel.Items {
-		v.Channel.Items[i].PreSave(v.Channel.Href, v.Channel.Category)
+		v.Channel.Items[i].PreProcess(v.Channel.Href, v.Channel.Category)
 	}
 	return v.Channel.Channel, v.Channel.Items
 }
