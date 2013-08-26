3706bd8ff523ec98cc9128c6d5b855dacff8198f
hearts <hearts.zhang@gmail.com>
2013-08-26 11:08:00 +0800

    redefine Insert.... functions
    - use Upsert to insert Channel and Entry
    - add feature SaveEntries
    - filter entries by link/guid/title
    - sort top-entries by -created, nor pubdate
    - top-entries will only return unread entries
    - add feature EntriesContentUnready
    - add feature EntryUpdateContent, EntryUpdateContent save the full-text
    doc file path to field entry.content_path
    - alter MarkRead....
    - remove Channel Entry's explicit _id struct memeber

--- feeds/db.go
+++ feeds/db.go
@@ -39,39 +39,47 @@ func DoInSession(collection string, act func(*mgo.Collection)) {
 	act(c)
 }
 
-func InsertChannel(c Channel) {
+func InsertChannel(c Channel) int {
+	rtn := 0
 	DoInSession("channels", func(coll *mgo.Collection) {
 		cond := bson.M{"link": c.Href}
-		_, err := coll.Upsert(cond, &c)
+		ci, err := coll.Upsert(cond, &c)
+		if ci.Updated > 0 {
+			log.Println("insert dup channel", c.Title, c.Href)
+		}
+		rtn = ci.Updated
 		try_panic(err)
 	})
+	return rtn
+}
+
+func insert_entry(coll *mgo.Collection, e Entry) {
+	cond := bson.M{
+		"$or": []bson.M{
+			{"link": e.Link},
+			{"guid": e.Guid},
+			{"title": e.Title}}}
+	ci, err := coll.Upsert(cond, &e)
+	if ci.Updated > 0 {
+		log.Println("insert dup entry", e.Link, e.Title, e.Guid)
+	}
+	try_panic(err)
 }
 
 func InsertEntry(e Entry) {
 	DoInSession("entries", func(coll *mgo.Collection) {
-		cond := bson.M{
-			"$or": []bson.M{
-				{"link": e.Link},
-				{"guid": e.Guid},
-				{"title": e.Title},
-				{"digest": e.Digest}}}
-		_, err := coll.Upsert(cond, &e)
-		try_panic(err)
+		insert_entry(coll, e)
 	})
 }
 
+func SaveEntries(channel Channel, entries []Entry) {
+	InsertEntries(entries)
+	InsertChannel(channel)
+}
 func InsertEntries(entries []Entry) {
 	DoInSession("entries", func(coll *mgo.Collection) {
 		for _, e := range entries {
-			cond := bson.M{"$or": []bson.M{{"link": e.Link},
-				{"guid": e.Guid},
-				{"title": e.Title},
-				{"digest": e.Digest}}}
-			ci, err := coll.Upsert(cond, &e)
-			if ci.Updated > 0 {
-				log.Println("insert dup entry", e.Link, e.Title, e.Digest)
-			}
-			try_panic(err)
+			insert_entry(coll, e)
 		}
 	})
 }
@@ -79,7 +87,7 @@ func InsertEntries(entries []Entry) {
 func TopNEntries(skip, limit int) []Entry {
 	set := []Entry{}
 	DoInSession("entries", func(coll *mgo.Collection) {
-		err := coll.Find(nil).Sort("-pubdate").Skip(skip).Limit(limit).All(&set)
+		err := coll.Find(bson.M{"read": false}).Sort("-created").Skip(skip).Limit(limit).All(&set)
 		try_panic(err)
 	})
 	return set
@@ -88,16 +96,28 @@ func TopNEntries(skip, limit int) []Entry {
 func TopNEntriesByCategory(category string, skip, limit int) []Entry {
 	set := []Entry{}
 	DoInSession("entries", func(coll *mgo.Collection) {
-		err := coll.Find(bson.M{"category": category}).Sort("-pubdate").Skip(skip).Limit(limit).All(&set)
+		err := coll.Find(bson.M{"category": category, "read": false}).Sort("-pubdate").Skip(skip).Limit(limit).All(&set)
 		try_panic(err)
 	})
 	return set
 }
 
+func EntriesContentUnready() []Entry {
+	set := []Entry{}
+	DoInSession("entries", func(coll *mgo.Collection) {
+		err := coll.Find(bson.M{"content_status": content_status_unresolved, "read": false}).All(&set)
+		if err != nil {
+			log.Println(err)
+		}
+	})
+	return set
+
+}
+
 func TopNEntriesByChannel(cid bson.ObjectId, skip, limit int) []Entry {
 	set := []Entry{}
 	DoInSession("entries", func(coll *mgo.Collection) {
-		err := coll.Find(bson.M{"cid": cid}).Sort("-pubdate").Skip(skip).Limit(limit).All(&set)
+		err := coll.Find(bson.M{"cid": cid, "read": false}).Sort("-pubdate").Skip(skip).Limit(limit).All(&set)
 		try_panic(err)
 	})
 	return set
@@ -130,6 +150,23 @@ func TouchChannel(href string, ttl int) {
 	})
 }
 
+/*
+	content_status_ready      // content has been stored in a file
+	content_status_failed     // fetch content failed
+	content_status_unavail    // fetch content ok, buy extracted-body is low quality
+	content_status_summary    // content stored in Entry.Summary
+	content_status_content    // content stored in Entry.Content
+*/
+func EntryUpdateContent(tf, link string) {
+	DoInSession("entries", func(coll *mgo.Collection) {
+		cs := content_status_failed
+		if len(tf) > 0 {
+			cs = content_status_ready
+		}
+		coll.Update(bson.M{"link": link}, bson.M{"$inc": bson.M{"content_status": cs, "content_path": tf}})
+	})
+}
+
 func MarkReadCatetoryaDayBefore(category string) {
 	deadline := time.Now().AddDate(0, 0, -1)
 	DoInSession("entries", func(coll *mgo.Collection) {
@@ -139,7 +176,7 @@ func MarkReadCatetoryaDayBefore(category string) {
 	})
 }
 
-func MarkReadChannelaDayBefore(cid bson.ObjectId) {
+func MarkReadChannelaDayBefore(cid string) {
 	deadline := time.Now().AddDate(0, 0, -1)
 	DoInSession("entries", func(coll *mgo.Collection) {
 		_, err := coll.UpdateAll(bson.M{"pubdate": bson.M{"$lt": deadline}, "cid": cid},
@@ -148,9 +185,9 @@ func MarkReadChannelaDayBefore(cid bson.ObjectId) {
 	})
 }
 
-func MarkReadEntry(id bson.ObjectId) {
+func MarkReadEntry(link string) {
 	DoInSession("entries", func(coll *mgo.Collection) {
-		err := coll.Update(bson.M{"_id": id},
+		err := coll.Update(bson.M{"link": link},
 			bson.M{"$inc": bson.M{"read": true}})
 		try_panic(err)
 	})
