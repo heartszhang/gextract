054952ec018dc2d2a0cb703adb8569379c17ca09
hearts <hearts.zhang@gmail.com>
2013-08-26 10:52:27 +0800

    add app subscribe
    subscribe --uri http://xxx.com.s/rss2.xml
    
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ subscribe/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	//	"fmt"
+	"gextract/document"
+	"gextract/feeds"
+	"log"
+	"os"
+)
+
+func init() {
+	log.SetOutput(os.Stderr)
+}
+
+var uri = flag.String("uri", "", "rss 2.0 url")
+
+func main() {
+	defer catch_panic()
+	flag.Parse()
+	if len(*uri) == 0 {
+		panic(errors.New("usage: subscribe --uri http://xome.rss2.xml"))
+	}
+	rsfile := document.FetchUrl2(*uri)
+	log.Println(rsfile)
+	ch, _ := feeds.NewRss2(rsfile, *uri)
+	log.Println(ch)
+	feeds.InsertChannel(ch)
+}
+
+func catch_panic() {
+	if e := recover(); e != nil {
+		log.Fatalln(e)
+	}
+}
