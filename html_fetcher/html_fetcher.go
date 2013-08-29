ddf345e558e2f86542adddade7d379d902f0abdf
hearts <hearts.zhang@gmail.com>
2013-08-30 07:53:59 +0800

    add html_fetcher
    html_fetcher download a uri and convert it to utf-8 encoded

---
+++ html_fetcher/html_fetcher.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	d "gextract/document"
+)
+
+var (
+	uri = flag.String("uri", "", "the online web page")
+)
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	flag.Parse()
+	if uri == nil || len(*uri) == 0 {
+		fmt.Println("url_fetcher --uri http://www.sina.com.cn")
+		return
+	}
+
+	fp, mt, _ := d.DownloadHtml(*uri)
+	fmt.Println(fp, mt)
+}
