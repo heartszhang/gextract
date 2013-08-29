87a82b9915029784edb1aa4b69b26c1b9d690a4a
hearts <hearts.zhang@gmail.com>
2013-08-30 07:48:27 +0800

    add db demo
    demo for TopN and SetContent

---
+++ db_demo/db_demo.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"gextract/feeds"
+)
+
+func main() {
+	/*
+		fo := feeds.NewFeedOperator()
+		fds, err := fo.TimeoutFeeds()
+		fmt.Println(len(fds), err)
+
+			for _, feed := range fds {
+				err := fo.Touch(feed.Link, feed.TTL)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+	*/
+	eo := feeds.NewEntryOperator()
+	etris, err := eo.TopN(0, 10)
+	fmt.Println(len(etris), err)
+	for _, entry := range etris {
+		err = eo.SetContent(entry.Link, "", 1, []string{"a.dumy", "b.dmy"})
+	}
+}
