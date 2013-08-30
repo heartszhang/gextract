c2ec6413c9be8477d63c1ad9e0f759793bc45901
hearts <hearts.zhang@gmail.com>
2013-08-30 08:02:14 +0800

    demo
    - provide basic rss file parsing demo
    - provide basic db demo

--- demo/demo.go
+++ demo/demo.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"gextract/feeds"
+	"gextract/feeds/rss"
 	"log"
 	"os"
 )
+
 var (
-	rssfile = "rss.xml"
+	rssfile  = "rss.xml"
 	base_url = "http://dummy.com"
 )
 
@@ -15,12 +17,22 @@ func init() {
 	log.SetOutput(os.Stderr)
 }
 func main() {
-	c, items := feeds.NewRss2(rssfile, base_url)
+	c, items, _ := rss.NewRss2(rssfile, base_url)
 	fmt.Println(c)
-	feeds.InsertChannel(c)
-	test_db(items)
+	for _, item := range items {
+		fmt.Println(item.Title)
+	}
+	fo := feeds.NewFeedOperator()
+	fo.Upsert(c)
+	x, _ := fo.Find("http://fulltextrssfeed.com/www.infzm.com/rss/home/rss2.0.xml")
+	fmt.Println(x)
+
+	eo := feeds.NewEntryOperator()
+	eo.Save(items)
+	//	feeds.InsertChannel(c)
+	//	test_db(items)
 }
 
 func test_db(items []feeds.Entry) {
-	feeds.InsertEntries(items)
+
 }
